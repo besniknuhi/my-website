b3f51c93280cfa49bb04988577619b34ab6355c2
unknown <nuhibesnik@gmail.com>
2024-07-12 23:40:33 +0200

    added new project

--- src/Components/Works/Data.jsx
+++ src/Components/Works/Data.jsx
@@ -1,5 +1,6 @@
 import ecomm from "../../assets/ecomm.jpg";
 import carRental from "../../assets/car-rental.jpg";
+import vps from "../../assets/vps-main.jpg";
 
 export const projectsData = [
   {
@@ -17,6 +18,14 @@ export const projectsData = [
     category: "web",
     tools: "js,react",
     url: "https://car-rental-bni.vercel.app/",
+  },
+  {
+    id: 3,
+    image: vps,
+    title: "Vps",
+    category: "web",
+    tools: "nextjs,ts,tailwind",
+    url: "https://vps-app2.vercel.app/",
   },
 ];
 
